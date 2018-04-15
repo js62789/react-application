0dcf7b7decb767ae58b9576bf9bb444b8ca213fb
Jeffrey Smith <js62789@gmail.com>
2018-04-15 13:42:14 -0400

    Export core modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 export { default as ReactApplication } from './ReactApplication';
+export { default as PropTypes } from 'prop-types';
+export { default as React } from 'react';
+export { connect } from 'react-redux';
+export { combineReducers } from 'redux';
+export { withRouter, Route, Link } from 'react-router-dom';
+export { hot } from 'react-hot-loader';
